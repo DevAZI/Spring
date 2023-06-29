8f9fa3620036f377df24e9d7790e857cc84c8451
DevAZI <80841284+DevAZI@users.noreply.github.com>
2023-06-29 14:23:55 +0000

    Main, Controller

---
+++ demo/src/main/controller/RestApiDemoController.java
@@ -0,0 +1,70 @@
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/coffee")
+class RestApiDemoController{
+    private List<Coffee> coffees = new ArrayList<>();
+
+    public RestApiDemoController(){
+        coffees.addAll(List.of( //addAll 값들을 전부 추가 List.of를 통하여 새로운 값들을 생성
+            new Coffee("Cafe Cereza"),
+            new Coffee("Cafe Ganaodr"),
+            new Coffee("Cafe Tres Pontas")
+
+        ));
+    }
+    // @RequestMapping(value = "/coffees", method = RequestMethod.GET) //getCoffee 메서드가 get요청의 url에만 응답하도록
+    // Iterable<Coffee> getCoffees(){
+    //     return coffees;
+    // }
+
+    // GetMapping으로 개선
+    @GetMapping
+    Iterable<Coffee> getCoffees(){
+        return coffees;
+    }
+
+    //Optional 값이 있거나 없을 때 사용 가능
+    // @PathVariable를 이용하여 /{id}의 값을 가져올 수 있음
+    @GetMapping("/{id}")
+    Optional<Coffee> getCoffeeById(@PathVariable String id){
+        for(Coffee c : coffees){
+            if(c.getId().equals){
+                return Optional.of(c);
+            }
+        }
+        return Optional.empty();
+    }
+    @PostMapping
+    Coffee postCoffee(@RequestBody Coffee coffee){
+        coffees.add(coffee);
+        return coffee;
+    }
+    @PutMapping("/{id}") //Put은 crud에서 update 라고 생각하면 될듯
+    Coffee putCoffee (@PathVariable String id , @RequestBody Coffee coffee){
+        int coffeeIndex = -1;
+        for(Coffee c: coffee){
+            if(c.getId().equals(id)){
+                coffeeIndex = coffees.indexOf(c);   //c  > 인덱스 위치 찾음
+                coffees.set(coffeeIndex , coffee);  //값 대체 (update)
+            }
+        }
+
+        //없으면 post 있으면 새로운 coffee 반환
+        //PUT은 응답시 상태코드 사용 필수!!
+        //DELETE 와 POST는 응답시 상태코드 사용 권장
+        return (coffeeIndex == -1) ? 
+            new ResponseEntity<>(postCoffee(coffee),HttpStatus.CREATED):    //200(OK)
+            new ResponseEntity<>(coffee, HttpStatus.OK);    //201(CREATED)
+
+
+        
+    }
+
+    @DeleeteMapping("/{id}")
+    void deleteCoffee(@PathVariable String id ){
+        coffees.removeIf(c-> c.getId().equals(id));     //값이 true면 요소 삭제
+    }
+
+    
+}
---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import java.util.UUID;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
+class Coffee{
+	private final String id; //특정 커피 종류의 고유 식별값	
+	private String name; //커피(종류) 이름
+
+	public Coffe (String id, String name){
+		this(UUID.randomUUID().toString().name);		
+		//무작위로 생성된 고유한 식별자를 반환합니다. toString() 메서드를 호출하여 UUID를 문자열로 변환
+		//ex) "cbb44562-7da4-4c0c-a5bc-57f84d0a01c3"
+	}
+	public String getId(){
+		return id;
+	}
+	public String getName(){
+		return name;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+}
+
