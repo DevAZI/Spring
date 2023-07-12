d98bc2e19e409f26f97bc4460c892a48cbbcfcbc
DevAZI <80841284+DevAZI@users.noreply.github.com>
2023-07-12 14:22:43 +0000

    ch5 begin2

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,19 +1,125 @@
 package com.example.demo;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
 
-
-
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @SpringBootApplication
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+@Component
+class DataLoader {
+	private final CoffeeRepository coffeeRepository;
 
+	public DataLoader(CoffeeRepository coffeeRepository) {
+		this.coffeeRepository = coffeeRepository;
+	}
+
+	@PostConstruct
+	private void loadData() {
+		coffeeRepository.saveAll(List.of(
+				new Coffee("Café Cereza"),
+				new Coffee("Café Ganador"),
+				new Coffee("Café Lareño"),
+				new Coffee("Café Três Pontas")
+		));
+	}
+}
+
+@RestController
+@RequestMapping("/coffees")
+class RestApiDemoController {
+	private final CoffeeRepository coffeeRepository;
+
+	public RestApiDemoController(CoffeeRepository coffeeRepository) {
+		this.coffeeRepository = coffeeRepository;
+	}
+
+	@GetMapping
+	Iterable<Coffee> getCoffees() {
+		return coffeeRepository.findAll();
+	}
+
+	@GetMapping("/{id}")
+	Optional<Coffee> getCoffeeById(@PathVariable String id) {
+		return coffeeRepository.findById(id);
+	}
+
+	@PostMapping
+	Coffee postCoffee(@RequestBody Coffee coffee) {
+		return coffeeRepository.save(coffee);
+	}
+
+	@PutMapping("/{id}")
+	ResponseEntity<Coffee> putCoffee(@PathVariable String id,
+									 @RequestBody Coffee coffee) {
+
+		return (coffeeRepository.existsById(id))
+				? new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.OK)
+				: new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.CREATED);
+	}
+
+	@DeleteMapping("/{id}")
+	void deleteCoffee(@PathVariable String id) {
+		coffeeRepository.deleteById(id);
+	}
+}
+
+interface CoffeeRepository extends CrudRepository<Coffee, String> {}
+
+@Entity
+class Coffee {
+	@Id
+	private String id;
+	private String name;
+
+	public Coffee() {
+	}
+
+	public Coffee(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Coffee(String name) {
+		this(UUID.randomUUID().toString(), name);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	}
 	@RestController
 	@RequestMapping("/greeting")
 	class GreetingController{
-		@Value("${greeting-name : Mirage}")
+		@Value("$greeting-name: Mirage")
 		private String name;
 
 		@GetMapping
@@ -21,10 +127,4 @@ public class DemoApplication {
 			return name;
 		}
 	}
-
-
 }
-
-
-
-
